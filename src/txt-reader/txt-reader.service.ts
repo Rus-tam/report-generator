09ae3e31226160f6d911ea2d4a77e9fb64f088f2
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-03-07 00:56:15 +0500

    tray number

--- src/txt-reader/txt-reader.service.ts
+++ src/txt-reader/txt-reader.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs/promises';
+import { start } from 'repl';
 import  { UtilsService } from '../utils/utils.service';
 
 @Injectable()
@@ -9,9 +10,10 @@ export class TxtReaderService {
 	async parseTXTFile() {
 		const data: string = await fs.readFile('src/files/internals.txt', 'utf8');
 		const lines = data.split('\n');
+
 		const numberOfTrays = await this.trayNumber(lines);
-		
-		console.log(numberOfTrays);
+
+		await this.trayEfficiencies(lines, numberOfTrays);
 	}
 
 	private async trayNumber(lines: string[]): Promise<number> {
@@ -37,5 +39,47 @@ export class TxtReaderService {
 		}
 
 		return Math.max.apply(Math, startEndTrays);
+	}
+
+	// Нужно рефакторить
+	private trayEfficiencies(lines: string[], numberOfTrays: number): string[] {
+		let startPosition = 0;
+		let endPosition = 0;
+		let workingRange: string[] = [];
+		let isCondenser = false;
+		let trayEfficiencies: string[] = [];
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i].includes('КПД ступеней')) {
+				startPosition = i;
+			};
+			if (lines[i].includes('Condenser')) {
+				isCondenser = true;
+			};
+		};
+
+		// В выгрузке КПД конденсатора и ребойлера включается в кпд ступеней.
+		// Тут идет корректировка выборки значений
+		isCondenser ? startPosition = startPosition + 3 : null;
+		endPosition = startPosition + numberOfTrays;
+
+		for (let i = startPosition; i < endPosition; i++) {
+			workingRange.push(lines[i]);
+		};
+
+		const splitedLines = this.utilsService.arrayElementSplit(workingRange, ' ');
+		const filteredLines = this.utilsService.deleteEmptyElements(splitedLines);
+
+		trayEfficiencies.push(filteredLines[0]);
+		for (let i = 0; i < filteredLines.length; i++) {
+			try {
+				if (filteredLines[i] === '\r') {
+					trayEfficiencies.push(filteredLines[i + 1]);
+				}
+			} catch (e) {
+				return null;
+			}
+		};
+
+		return trayEfficiencies;
 	}
 }
