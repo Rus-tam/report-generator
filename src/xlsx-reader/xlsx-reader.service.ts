a0971ac874903fa912cac34d9f69ec399817732e
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-03-17 15:57:37 +0500

    some refactoring-2

--- src/xlsx-reader/xlsx-reader.service.ts
+++ src/xlsx-reader/xlsx-reader.service.ts
@@ -19,18 +19,91 @@ export class XlsxReaderService {
     const { feedProperties, drawProperties } = this.streamProperties(materialStreams, txtData);
   }
 
+  // Извлечение составов из экселевского документа
+  private streamCompositionExtractor(stages: {}, compositions: {}[]): {} {
+    let molFraction = {};
+    let streamComposition = {};
+    const streams = this.utilsService.objectKeyFinder(stages).filter((stream) => {
+      if (
+        !stream.includes("Reboiler") &&
+        !stream.includes("Condenser") &&
+        !stream.includes("Boilup") &&
+        !stream.includes("Reflux")
+      ) {
+        return stream;
+      }
+    });
+
+    for (let stream of streams) {
+      molFraction = {};
+      for (let obj of compositions) {
+        if (obj[stream] >= 0.000001) {
+          molFraction[obj["__EMPTY"]] = obj[stream];
+        } else if (obj[stream] === undefined) {
+          null;
+        } else {
+          molFraction[obj["__EMPTY"]] = 0;
+        }
+      }
+      streamComposition[stream] = molFraction;
+    }
+
+    return streamComposition;
+  }
+
+  // Извлечение свойств потоков из экселевского документа
+  private streamPropertiesExtractor(stages: {}, properties: {}[]): {} {
+    const contents = [
+      "Vapour Fraction",
+      "Temperature [C]",
+      "Pressure [MPa]",
+      "Molar Flow [kgmole/h]",
+      "Mass Flow [kg/h]",
+      "Heat Flow [MW]",
+      "Molecular Weight",
+      "Mass Density [kg/m3]",
+      "Vapour Volume Flow [m3/h]",
+      "Liquid Volume Flow [m3/h]",
+    ];
+    let propData = {};
+    let streamProperties = {};
+    const streams = this.utilsService.objectKeyFinder(stages);
+
+    // Из-за проблем с кодировкой заголовки строчей не прочитываются. Соответственно их нужно заменить
+    for (let i = 0; i < contents.length; i++) {
+      properties[i]["__EMPTY"] = contents[i];
+    }
+
+    for (let stream of streams) {
+      propData = {};
+      for (let obj of properties) {
+        if (obj["__EMPTY"] === contents[9] && obj[stream] !== "<empty>") {
+          propData[contents[9]] = obj[stream] * 3600;
+        } else if (obj["__EMPTY"] === contents[9] && obj[stream] === "<empty>") {
+          propData[contents[9]] = 0;
+        } else if (obj["__EMPTY"] !== contents[9] && obj[stream] !== "<empty>") {
+          propData[obj["__EMPTY"]] = obj[stream];
+        } else if (obj["__EMPTY"] !== contents[9] && obj[stream] === "<empty>") {
+          propData[obj["__EMPTY"]] = 0;
+        }
+      }
+      streamProperties[stream] = propData;
+    }
+    return streamProperties;
+  }
+
   private streamCompositions(compositions: {}[], txtData: ITxtData): IStreamComposition {
     const { feedStages, drawStages, ...rest } = txtData;
-    const drawCompositions = this.utilsService.streamComposition(drawStages, compositions);
-    const feedCompositions = this.utilsService.streamComposition(feedStages, compositions);
+    const drawCompositions = this.streamCompositionExtractor(drawStages, compositions);
+    const feedCompositions = this.streamCompositionExtractor(feedStages, compositions);
 
     return { feedCompositions, drawCompositions };
   }
 
   private streamProperties(materialStreams: {}[], txtData: ITxtData): IStreamProperty {
     const { feedStages, drawStages, ...rest } = txtData;
-    const feedProperties = this.utilsService.streamProperty(feedStages, materialStreams);
-    const drawProperties = this.utilsService.streamProperty(drawStages, materialStreams);
+    const feedProperties = this.streamPropertiesExtractor(feedStages, materialStreams);
+    const drawProperties = this.streamPropertiesExtractor(drawStages, materialStreams);
 
     return { feedProperties, drawProperties };
   }
